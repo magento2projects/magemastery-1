44208db32a8e47ebf656b904bc84fa991492a093
Stanislav Pomaleyko <stanislav.pomaleyko@gmail.com>
2020-04-21 11:12:48 +0300

    Lesson 32: Creating first task with Model and Resource Model

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -4,13 +4,36 @@ declare(strict_types=1);
 
 namespace MageMastery\Todo\Controller\Index;
 
+use MageMastery\Todo\Model\ResourceModel\Task as TaskResource;
+use MageMastery\Todo\Model\Task;
+use MageMastery\Todo\Model\TaskFactory;
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
 {
+    private $taskResource;
+
+    private $taskFactory;
+
+    public function __construct(Context $context, TaskFactory $taskFactory, TaskResource $task)
+    {
+        $this->taskFactory = $taskFactory;
+        $this->taskResource = $task;
+        parent::__construct($context);
+    }
+
     public function execute()
     {
+        $task = $this->taskFactory->create();
+        $task->setData([
+            'label' => 'New Task 3',
+            'status' => 'open',
+            'customer_id' => 1
+        ]);
+        $this->taskResource->save($task);
+
         return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
