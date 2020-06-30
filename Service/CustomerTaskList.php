85839137a825a89e9b3acce1ddf98f9a29dda10d
Stanislav Pomaleyko <stanislav.pomaleyko@amasty.com>
2020-06-30 20:52:46 +0300

    Bonus Lesson 61: Adding a Customer ID filter to the list of tasks

--- Service/CustomerTaskList.php
+++ Service/CustomerTaskList.php
@@ -4,7 +4,9 @@ namespace MageMastery\Todo\Service;
 
 use MageMastery\Todo\Api\Data\TaskInterface;
 use MageMastery\Todo\Api\TaskRepositoryInterface;
-use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Customer\Model\Session;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\Search\SearchCriteriaBuilder;
 
 class CustomerTaskList implements \MageMastery\Todo\Api\CustomerTaskListInterface
 {
@@ -12,17 +14,31 @@ class CustomerTaskList implements \MageMastery\Todo\Api\CustomerTaskListInterfac
      * @var TaskRepositoryInterface
      */
     private $taskRepository;
+
     /**
      * @var SearchCriteriaBuilder
      */
     private $searchCriteriaBuilder;
 
+    /**
+     * @var FilterBuilder
+     */
+    private $filterBuilder;
+    /**
+     * @var Session
+     */
+    private $session;
+
     public function __construct(
         TaskRepositoryInterface $taskRepository,
-        SearchCriteriaBuilder $searchCriteriaBuilder
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        FilterBuilder $filterBuilder,
+        Session $session
     ) {
         $this->taskRepository = $taskRepository;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->filterBuilder = $filterBuilder;
+        $this->session = $session;
     }
 
     /**
@@ -30,8 +46,15 @@ class CustomerTaskList implements \MageMastery\Todo\Api\CustomerTaskListInterfac
      */
     public function getList()
     {
+        $this->searchCriteriaBuilder->addFilter(
+            $this->filterBuilder->create()
+                ->setField('customer_id')
+                ->setValue($this->session->getCustomerId())
+        );
+        $searchCriteria = $this->searchCriteriaBuilder->create();
+
         return $this->taskRepository
-            ->getList($this->searchCriteriaBuilder->create())
+            ->getList($searchCriteria)
             ->getItems();
     }
 }
