483b153e79c886f82c44952e62457f02f3d79caf
Stanislav Pomaleyko <stanislav.pomaleyko@amasty.com>
2020-07-12 10:51:51 +0300

    Bonus Lesson 62: Verifying if a user is logged in to see the TODO list

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -4,52 +4,35 @@ declare(strict_types=1);
 
 namespace MageMastery\Todo\Controller\Index;
 
-use MageMastery\Todo\Api\TaskManagementInterface;
-use MageMastery\Todo\Model\ResourceModel\Task as TaskResource;
-use MageMastery\Todo\Model\Task;
-use MageMastery\Todo\Model\TaskFactory;
-use MageMastery\Todo\Service\TaskRepository;
-use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Customer\Model\Session;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
+use Magento\Framework\Controller\Result\Redirect;
 use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
 {
-    private $taskResource;
+    /**
+     * @var Session
+     */
+    private $session;
 
-    private $taskFactory;
-    /**
-     * @var TaskRepository
-     */
-    private $taskRepository;
-    /**
-     * @var SearchCriteriaBuilder
-     */
-    private $searchCriteriaBuilder;
-    /**
-     * @var TaskManagementInterface
-     */
-    private $taskManagement;
-
-    public function __construct(
-        Context $context,
-        TaskFactory $taskFactory,
-        TaskResource $task,
-        TaskRepository $taskRepository,
-        SearchCriteriaBuilder $searchCriteriaBuilder,
-        TaskManagementInterface $taskManagement
-    ) {
-        $this->taskFactory = $taskFactory;
-        $this->taskResource = $task;
-        $this->taskRepository = $taskRepository;
-        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
-        $this->taskManagement = $taskManagement;
+    public function __construct(Context $context, Session $session)
+    {
         parent::__construct($context);
+        $this->session = $session;
     }
 
     public function execute()
     {
+        if (!$this->session->isLoggedIn()) {
+            /** @var Redirect $redirect */
+            $redirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+            $redirect->setPath('customer/account/login');
+
+            return $redirect;
+        }
+
         return $this->resultFactory->create(ResultFactory::TYPE_PAGE);
     }
 }
